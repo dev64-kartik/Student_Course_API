9330e3a01a2c59a2e196b6b4ed25b0ebab389bb6
Kartik Patel <kp481216@gmail.com>
2023-02-07 11:43:59 +0530

    Migrate to intellij idea

--- src/main/java/com/example/api/StudentCourseMaster.java
+++ src/main/java/com/example/api/StudentCourseMaster.java
@@ -2,13 +2,12 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class StudentCourseManagementApiApplication {
+public class StudentCourseMaster {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentCourseManagementApiApplication.class, args);
+		SpringApplication.run(StudentCourseMaster.class, args);
 	}
 
 }
